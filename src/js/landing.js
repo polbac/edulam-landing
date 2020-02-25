137c3b8478e66da13aeacc3402c3fd71cadde4b2
Pablo Bacchetta <polbac@gmail.com>
2020-02-25 20:57:52 -0300

    more

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,9 +1,35 @@
 function init() {
     $(window).scroll(checkScroll);
+    $(window).scroll(checkScrollFeatures);
 };
 
 function checkScroll() {
+    var scrollY = $(document).scrollTop();
 
+    if (scrollY > 100) {
+        $("header").addClass("small");
+        return;
+    }
+
+    $("header").removeClass("small");
+}
+
+function checkScrollFeatures() {
+    var scrollY = $(document).scrollTop();
+    var featuresOffsetY = $("section.features").offset().top;
+    var featuresHeight = $("section.features").height() + $(window).height();
+    var containerWidth = $(window).width() > 1140 ? 1140 : $(window).width();
+
+    if (scrollY > featuresOffsetY && scrollY < (featuresOffsetY + featuresHeight)) {
+        $(".fixed-block")
+            .addClass("fixed")
+            .css({
+                left: ($(window).width() / 2) - (containerWidth / 2) + 15,
+            });        
+        return;
+    }
+
+    $(".fixed-block").removeClass("fixed").css({ left: 0 });
 }
 
 $(document).ready(init);
